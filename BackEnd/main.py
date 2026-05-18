d056dbca5929564ab673751919393160ffadcb4a
Dhanush Krishna K <85447114+Dhanushkarthik29@users.noreply.github.com>
2026-05-18 09:30:32 +0530

    Add files via upload

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -5,14 +5,10 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import os
 
-
-print("Python file is working fine");
-# Load environment variables
 load_dotenv()
-# FastAPI app
+
 app = FastAPI()
 
-# Enable CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,38 +16,28 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-print(os.getenv("GROQ_API_KEY"))
-# Groq Client
+
 client = OpenAI(
     api_key=os.environ.get("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1"
 )
 
-# Request model
 class ChatRequest(BaseModel):
     messages: list
 
+@app.get("/")
+async def root():
+    return {"status": "ok", "message": "Smart AI Chat Backend is running"}
 
 @app.post("/smart-ai-chat")
 async def chat(req: ChatRequest):
-
     try:
-
         response = client.chat.completions.create(
             model="llama-3.3-70b-versatile",
-
             messages=req.messages,
-
             temperature=0.7,
             max_tokens=1000
         )
-
-        return {
-            "reply": response.choices[0].message.content
-        }
-
+        return {"reply": response.choices[0].message.content}
     except Exception as e:
-
-        return {
-            "error": str(e)
-        }
+        return {"error": str(e)}
